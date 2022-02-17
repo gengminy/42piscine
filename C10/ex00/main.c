1a065258ad1906e32bba521514556dfb21dfa302
Kyeongmin Noh <knoh@c10r7s7.42seoul.kr>
2022-02-17 19:54:17 +0900

    0217

--- C10/ex00/main.c
+++ C10/ex00/main.c
@@ -1,28 +1,54 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   validate.c                                         :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: knoh <knoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/15 19:42:43 by knoh              #+#    #+#             */
-/*   Updated: 2022/02/15 19:42:44 by knoh             ###   ########.fr       */
+/*   Created: 2022/02/15 14:12:52 by knoh              #+#    #+#             */
+/*   Updated: 2022/02/15 14:12:53 by knoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <fcntl.h>
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
 
 int	ft_is_arg_valid(int argc)
 {
 	if (argc < 2)
 	{
-		write(1, "File name missing.\n", 19);
+		ft_putstr("File name missing.\n");
 		return (0);
 	}
 	else if (argc > 2)
 	{
-		write(1, "Too many arguments.\n", 20);
+		ft_putstr("Too many arguments.\n");
 		return (0);
 	}
 	return (1);
+}
+
+int	main(int argc, char *argv[])
+{
+	 int	file;
+	char	buffer[1024];
+
+	if (ft_is_arg_valid(argc))
+	{
+		file = open(argv[1], O_RDONLY);
+		if(file < 0)
+			ft_putstr("Cannot read file.\n");
+		else
+		{
+			if (read(file, buffer, 1024) >= 0)
+				ft_putstr(buffer);
+		}
+		close(file);
+	}
 }
--- C10/ex01/main.c
+++ C10/ex01/main.c
@@ -11,28 +11,73 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <string.h>
 #include <fcntl.h>
+#include <errno.h>
+#include <libgen.h>
 
-#include <stdio.h>
+#define BUFFER_SIZE 1
 
-int	ft_is_arg_valid(int argc);
+char	*g_program;
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+void	print_err_msg(char *file)
+{
+	ft_putstr(basename(g_program));
+	ft_putstr(": ");
+	ft_putstr(file);
+	ft_putstr(": ");
+	ft_putstr(strerror(errno));
+	ft_putstr("\n");
+	errno = 0;
+}
+
+void	display_file(int fd, char *file)
+{
+	int				size;
+	unsigned char	buffer[BUFFER_SIZE];
+
+	while (1)
+	{
+		size = read(fd, buffer, BUFFER_SIZE);
+		if (errno)
+		{
+			print_err_msg(file);
+			return ;
+		}
+		if (size == 0)
+			return ;
+		write(1, buffer, size);
+	}
+}
 
 int	main(int argc, char *argv[])
 {
-	 int	file;
-	 int	length;
-	char	buffer[1024];
+	int	fd;
+	int	i;
 
-	if (ft_is_arg_valid(argc))
+	g_program = argv[0];
+	if (argc == 1)
+		display_file(0, 0);
+	else
 	{
-		file = open(argv[1], O_RDONLY);
-		printf("file: %d\n", file);
-		if(file < 0)
-			write(1, "Cannot read file.\n", 18);
-		else
+		i = 0;
+		while (++i < argc)
 		{
-			read(file, buffer, length);
+			fd = open(argv[i], O_RDONLY);
+			if (fd == -1)
+				print_err_msg(argv[i]);
+			else
+			{
+				display_file(fd, argv[i]);
+				close(fd);
+			}
 		}
-		close(file);
 	}
+	return (0);
 }
