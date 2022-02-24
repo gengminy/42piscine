70d998ed87633ab6b868c378b8ea0444034d58d5
Kyeongmin Noh <knoh@c8r4s4.42seoul.kr>
2022-02-24 12:10:41 +0900

    1st try [100%]

---
+++ C11/ex06/ft_sort_string_tab.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knoh <knoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/15 15:58:57 by knoh              #+#    #+#             */
+/*   Updated: 2022/02/15 15:58:57 by knoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int				i;
+	unsigned char	c1;
+	unsigned char	c2;
+
+	i = 0;
+	while (s1[i] || s2[i])
+	{
+		c1 = s1[i];
+		c2 = s2[i];
+		if (c1 != c2)
+			return (c1 - c2);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	ft_sort_string_tab(char **tab)
+{
+	 int	i;
+	 int	j;
+	char	*tmp;
+
+	i = 0;
+	while (tab[i])
+	{
+		j = 0;
+		while (tab[j] && tab[j + 1])
+		{
+			if (ft_strcmp(tab[j], tab[j + 1]) > 0)
+			{
+				tmp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = tmp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
