c8ab8d95918b2927bad88d20f6bf80bfb4f12676
Kyeongmin Noh <knoh@c8r4s4.42seoul.kr>
2022-02-24 12:03:16 +0900

    1st try [100%]

---
+++ C00/ex08/ft_print_combn.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: knoh <knoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/05 15:41:27 by knoh              #+#    #+#             */
+/*   Updated: 2022/02/05 15:41:30 by knoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	g_num[10];
+
+void	ft_track_number(char prev, int cur_depth, int depth)
+{
+	char	cur;
+
+	if (cur_depth == depth)
+	{
+		write(1, g_num, depth);
+		if (g_num[0] != 10 - depth + '0')
+			write(1, ", ", 2);
+		return ;
+	}
+	cur = prev + 1;
+	while (cur <= '9')
+	{
+		g_num[cur_depth] = cur;
+		ft_track_number(cur, cur_depth + 1, depth);
+		cur++;
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	char	cur;
+
+	cur = '0';
+	g_num[n] = '\0';
+	while (cur <= '9')
+	{
+		g_num[0] = cur;
+		ft_track_number(cur, 1, n);
+		cur++;
+	}
+}
